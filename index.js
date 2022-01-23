d0457b2a60481e604508cffa345df01de71d7825
waltthesalt <97415670+waltthesalt@users.noreply.github.com>
2022-01-23 16:01:21 -0330

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+import Game from "./scenes/game.js";
+
+const config = {
+    type: Phaser.AUTO,
+    parent: 'game',
+    width: 1280,
+    height: 900,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_HORIZONTALLY
+    },
+    backgroundColor: '#999999',
+    dom: {
+        createContainer: true
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 }
+        }
+    },    
+    scene: [
+        Game
+    ] 
+};
+
+const game = new Phaser.Game(config);
+
